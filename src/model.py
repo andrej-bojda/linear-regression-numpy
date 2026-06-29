f6cc7a81288c216e8fd68814f4df3c263e94db21
Andrej Bojda <andrejbojdask@gmail.com>
2026-06-29 18:01:39 +0200

    add compute_gradients

--- src/model.py
+++ src/model.py
@@ -6,10 +6,17 @@ def predict(X,w,b):
 def compute_loss(y,y_pred):
     return np.mean((y - y_pred)**2)
 
+def compute_gradients(X, y, y_pred):
+    return np.mean(-2 * (y - y_pred) * X), np.mean(-2 * (y - y_pred))
+
 if __name__ == "__main__":
     from src.data import generate_data
-
     X, y, w_true, b_true = generate_data(2.5, 1.0, 100, 0.3, 42)
-    w = np.zeros(1)
+    w = np.zeros((1,1))
+    b = 0.0
     y_pred = predict(X, w , 0.0)
-    print(compute_loss(y, y_pred))
+    dw, db = compute_gradients(X, y, y_pred)
+    print(f"dw : {dw}")
+    print(f"db : {db}")
+
+
